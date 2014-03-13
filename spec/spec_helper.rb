6d7ad53cb4d3e6e05415ba6daf5a7a15b174cbeb
trilitheus <trilitheus@gmail.com>
2014-03-13 22:15:13 +0000

    Updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ require 'chefspec/berkshelf'
 RSpec.configure do |config|
   config.color_enabled = true
   config.tty = true
+  config.log_level = :error
   config.formatter = :documentation
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
